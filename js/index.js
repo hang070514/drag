a59edc96da6877de342fbbcc44414e3c2e6f2af5
zhang <hangisoft@163.com>
2018-03-01 16:07:41 +0800

    实现拖动改变宽度

--- js/index.js
+++ js/index.js
@@ -1,17 +1,65 @@
 $(function () {
     var isDown = false;
-    $('.left').on('mousedown','.rightBorder',function () {
-        console.log('down事件');
+    //左边原始宽度
+    var leftWidth = $('.left').width();
+    console.log('leftWidth===='+leftWidth);
+    //鼠标按下的x坐标
+    var downX = 0;
+    $('.left').on('mousedown','.rightBorder',function (e) {
+        console.log('down事件'+e.pageX);
         isDown = true;
+        downX = e.pageX;
     })
-    $(document).on('mousemove',function () {
-        if(isDown){
-            console.log('move事件');
-        }
+
+    $('.left').on('mouseenter','.rightBorder',function (e) {
+        console.log('enter事件'+e.pageX);
+    })
+    $('.left').on('mouseleave','.rightBorder',function (e) {
+        console.log('leave事件'+e.pageX);
+       /* if(Math.abs(e.pageX - downX)){
+            isDown = false;
+        }*/
 
     })
-    $(document).on('mouseup','.rightBorder',function(){
+
+    $(document).on('mousemove',function (e) {
+        if(isDown){
+            if(e.pageX - downX >= 0){
+                //向右移动
+                if($('.left').width() < leftWidth){
+                    var distance = e.pageX - downX;
+                    console.log('move事件右移if'+e.pageX);
+                    console.log('left==='+$('.rightBorder').position().left);
+                    $('.rightBorder').css('left',$('.rightBorder').position().left+distance);
+                    $('.left').width($('.left').width()+distance);
+                }else{
+                    console.log('move事件右移else width'+$('.left').width());
+                    console.log('move事件右移else'+e.pageX);
+                    $('.rightBorder').css('left',$('.left').width() - $('.rightBorder').width());
+                    $('.left').width(leftWidth);
+                }
+            }else{
+                //向左移动
+                console.log('move事件左移'+e.pageX);
+                if($('.left').width() > $('.rightBorder').width()){
+                    $('.rightBorder').css('left',e.pageX);
+                    $('.left').width(e.pageX);
+                }else{
+                    $('.rightBorder').css('left',$('.rightBorder').width());
+                    $('.left').width($('.rightBorder').width());
+                }
+
+            }
+
+
+        }
+    })
+
+
+    $(document).on('mouseup',function(){
         isDown = false;
-        console.log('up事件');
+        console.log('up事件12');
     });
+
+    //console.log('url==='+url);
 })
